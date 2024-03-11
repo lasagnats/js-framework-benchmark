18a0c9d8b46639b28aff169b94b9312e9e9d17d9
lasagnats <lasagna.tss@gmail.com>
2024-03-11 19:17:36 +0200

    Optimize Kotlin

--- frameworks/keyed/kotlin/build.gradle.kts
+++ frameworks/keyed/kotlin/build.gradle.kts
@@ -32,7 +32,7 @@ kotlin {
             }
 
             // Uncomment the next line to apply Binaryen and get optimized wasm binaries
-            // applyBinaryen()
+            applyBinaryen()
         }
     }
     sourceSets {
